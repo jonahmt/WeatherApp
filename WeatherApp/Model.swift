5a0359a27ac693a41e6c3565d81d03513805b8c4
Jonah Tharakan <jonahmt@berkeley.edu>
2022-08-16 18:17:30 -0700

    Did basic setup, like creating label variables, setting the background color, and created the Model class.

---
+++ WeatherApp/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  WeatherApp
+//
+//  Created by Jonah Tharakan on 8/16/22.
+//
+
+import Foundation
+
+class Model {
+    
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,10 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // UI Elements:
+    
+    lazy var locationLabel: UILabel = {
+        let l = UILabel()
+        l.textAlignment = .center
+        l.font = UIFont(name: "Arial", size: 30)
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+    
+    lazy var temperatureLabel: UILabel = {
+        let l = UILabel()
+        l.textAlignment = .center
+        l.font = UIFont(name: "Arial", size: 30)
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
+        setupConstraints()
+    }
+    
+    
+    func setupView() {
+        view.backgroundColor = UIColor(red: 147/255, green: 212/255, blue: 245/255, alpha: 1.0)
+        
+        view.addSubview(locationLabel)
+        view.addSubview(temperatureLabel)
+    }
+    
+    func setupConstraints() {
+        locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        locationLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        locationLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        locationLabel.heightAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        temperatureLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        temperatureLabel.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 500).isActive = true
+        temperatureLabel.widthAnchor.constraint(equalToConstant: 400).isActive = true
+        temperatureLabel.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
 
 
